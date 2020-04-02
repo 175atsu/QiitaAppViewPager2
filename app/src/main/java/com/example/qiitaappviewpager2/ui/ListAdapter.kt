86907c590a47170ff3cfa400a641edbd9f91318a
Atsu <astu.i.0601@gmail.com>
2020-04-03 00:53:14 +0900

    adapter周り作成

--- app/src/main/java/com/example/qiitaappviewpager2/ui/ListAdapter.kt
+++ app/src/main/java/com/example/qiitaappviewpager2/ui/ListAdapter.kt
@@ -2,19 +2,24 @@ package com.example.qiitaappviewpager2.ui
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.qiitaappviewpager2.R
 import com.example.qiitaappviewpager2.data.QiitaResponse
 
-class ListAdapter(parent: ViewGroup?) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ListAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private val data = mutableListOf<QiitaResponse>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        return ListViewHolder()
+        return ListViewHolder(DataBindingUtil.inflate(layoutInflater, R.layout.view_list_cell, parent, false))
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as ListViewHolder).apply {
+            bind(data[position])
+        }
     }
 
     override fun getItemCount(): Int = data.size
--- app/src/main/java/com/example/qiitaappviewpager2/ui/ListViewHolder.kt
+++ app/src/main/java/com/example/qiitaappviewpager2/ui/ListViewHolder.kt
@@ -1,9 +1,14 @@
 package com.example.qiitaappviewpager2.ui
 
 import androidx.recyclerview.widget.RecyclerView
+import com.example.qiitaappviewpager2.data.QiitaResponse
+import com.example.qiitaappviewpager2.databinding.ViewListCellBinding
 
-class ListViewHolder (private val binding: ): RecyclerView.ViewHolder() {
-    fun bind() {
-
+class ListViewHolder (private val binding: ViewListCellBinding): RecyclerView.ViewHolder(binding.root) {
+    fun bind(qiitaResponse: QiitaResponse) {
+        this.commonBind(qiitaResponse)
+    }
+    fun commonBind(qiitaResponse: QiitaResponse) {
+        binding.webView.loadUrl(qiitaResponse.url)
     }
 }
